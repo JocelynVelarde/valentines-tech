ae646309e370ceac3ae7cb520dddb75b48bfddbb
JocelynVelarde <a01285780@itesm.mx>
2025-02-12 15:59:59 -0600

    added constants for auth in mongodb

--- mongo_test.py
+++ mongo_test.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://pollogunpao:oIgsSDA9xrDhujjM@cluster0.ny8favk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://pollogunpao:<db_password>@cluster0.ny8favk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
